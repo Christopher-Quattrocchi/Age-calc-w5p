a5ad6ccabfdbe84f76fdaabf89fe56094b699789
Christopher Quattrocchi <chrisquattrocchi@gmail.com>
2023-11-11 10:10:56 -0800

    created variables for each planet. Ready to create a test that will fail for mercuryCalculator

--- src/js/age.js
+++ src/js/age.js
@@ -1,5 +1,18 @@
 export default class Age {
   constructor(earthAge) {
     this.earthAge = earthAge;
+    this.mercuryAge = null;
+    this.venusAge = null;
+    this.marsAge = null;
+    this.jupiterAge = null;
+    this.saturnAge = null;
+    this.uranusAge = null;
+    this.neptuneAge = null;
+    this.plutoAge = null;
+  }
+
+  mercuryCalculator() {
+    // this.mercuryAge = (this.earthAge / 0.24);
+    return this;
   }
 }
